0a58bb7e4c04c05ad31d3af7a007ad08e39a345e
jametl <jametl@ttu.ee>
2023-01-31 11:21:58 +0200

    Test

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -9,7 +9,7 @@ export default function Menu(){
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item"> 
                             <NavLink className="nav-link" to="/genres">
-                                Genres
+                                Genressss
                             </NavLink>
                         </li>
                         <li className="nav-item"> 
