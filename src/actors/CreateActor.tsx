3a833727538fd8b7de0e651482869fa5513767e7
jametl <jametl@ttu.ee>
2023-02-28 15:13:17 +0200

    Added actor form and image upload funcionality

--- src/actors/CreateActor.tsx
+++ src/actors/CreateActor.tsx
@@ -1,8 +1,12 @@
+import ActorForm from "./ActorForm";
 
 export default function CreateActor(){
     return (
         <>
         <h3>Create actor</h3>
+        <ActorForm model={{name: '', dateOfBirth: undefined}}
+            onSubmit={values => console.log(values)
+            }/>
         </>
     )
 }
--- src/actors/EditActor.tsx
+++ src/actors/EditActor.tsx
@@ -1,7 +1,15 @@
+import ActorForm from "./ActorForm"
+
 export default function EditActor(){
     return(
         <>
         <h3>Edit actor</h3>
+        <ActorForm model={{name: 'Bryan Cranston',
+        dateOfBirth: new Date('1962-03-04T00:00:00'),
+        pictureURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Bryan_Cranston_at_the_2018_Berlin_Film_Festival_%282%29.jpg/440px-Bryan_Cranston_at_the_2018_Berlin_Film_Festival_%282%29.jpg'
+        }}
+            onSubmit={values => console.log(values)
+            }/>
         </>
     )
 }
